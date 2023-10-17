0f0657d7793a7356f782905d69e572d8bd365b74
오현서 <ohyeonseo@ohyeonseoui-MacBookAir.local>
2023-10-17 21:33:32 +0900

    huffman 초기커밋

--- huffman_code/huffman.c
+++ huffman_code/huffman.c
@@ -35,7 +35,7 @@ void insert_min_heap(HeapType* h,element item){
     int i;
     i = ++(h->heap_size);
 
-    while((i!=1) && (item.key < h->heap[i/2].key)){
+    while((i != 1) && (item.key < h->heap[i/2].key)){
         h->heap[i] = h->heap[i/2];
         i/=2;
     }
